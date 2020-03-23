0b4a878731fc1f1bf6c436a5c6b4c4e36d9f0b3a
Federico Vitale <fedevitale99@gmail.com>
2020-03-23 20:39:13 +0100

    plain array

--- api/_util/agent.ts
+++ api/_util/agent.ts
@@ -1,6 +1,8 @@
 import fetch from 'isomorphic-unfetch';
 import groupBy from 'lodash/groupBy';
-import isEqual from'lodash/isEqual'
+import first from 'lodash/first';
+import last from 'lodash/last';
+import isEqual from 'lodash/isEqual';
 
 import { ProvinceData, NationalData, RegionData, Dict } from './typings';
 
@@ -10,9 +12,8 @@ type NationalResponse = NationalData[];
 
 const ENDPOINT = `https://api.github.com/repos/pcm-dpc/COVID-19/contents`;
 
-
 const decodeBase64 = (base64: string) => Buffer.from(base64, 'base64').toString();
-const equals = (a:string, b:string) : boolean => isEqual(a.trim().toLowerCase(), b.trim().toLowerCase())
+const equals = (a: string, b: string): boolean => isEqual(a.trim().toLowerCase(), b.trim().toLowerCase());
 
 export const fetchRegionsData = async (): Promise<RegionsResponse> => {
 	const response = await fetch(ENDPOINT + '/dati-json/dpc-covid19-ita-regioni.json');
@@ -23,39 +24,50 @@ export const fetchRegionsData = async (): Promise<RegionsResponse> => {
 	return groupedByRegion;
 };
 
-export const fetchRegionData = (regionName:string) => async (): Promise<ProvincesResponse> => {
+export const fetchRegionsDataSimple = async () => {
+	const response = await fetch(ENDPOINT + '/dati-json/dpc-covid19-ita-regioni.json');
+	const data = await response.json();
+	const content: RegionData[] = JSON.parse(decodeBase64(data.content));
+	const groupedByRegion: RegionsResponse = groupBy(content, 'denominazione_regione');
+
+	return Object.keys(groupedByRegion).map((k) =>
+		first(groupedByRegion[k].sort((a, b) => new Date(a.data).getTime() - new Date(b.data).getTime())),
+	);
+};
+
+export const fetchRegionData = (regionName: string) => async (): Promise<ProvincesResponse> => {
 	const apiResponse = await fetch(ENDPOINT + `/dati-json/dpc-covid19-ita-province.json`);
 	const data = await apiResponse.json();
 
 	let content: ProvinceData[] = JSON.parse(decodeBase64(data.content));
 	content = content.filter((item) => item.denominazione_provincia !== 'In fase di definizione/aggiornamento');
-	const regionData = content.filter(el => equals(el.denominazione_regione, regionName) || equals(`${el.codice_regione}`, regionName));
+	const regionData = content.filter(
+		(el) => equals(el.denominazione_regione, regionName) || equals(`${el.codice_regione}`, regionName),
+	);
 
 	let response: any = {
 		status: 404,
 		title: 'Not Found',
-		message: 'Region not found'
+		message: 'Region not found',
 	};
 
 	if (regionData.length) {
-		response = regionData
+		response = regionData;
 	}
-
 
 	return response;
 };
 
-export const fetchProvinceData = (provinceName:string) => async (): Promise<ProvinceData[]> => {
+export const fetchProvinceData = (provinceName: string) => async (): Promise<ProvinceData[]> => {
 	const response = await fetch(ENDPOINT + `/dati-json/dpc-covid19-ita-province.json`);
 	const data = await response.json();
 
-	return JSON
-		.parse(decodeBase64(data.content))
-		.filter(el => 
-			equals(`${el.codice_provincia}`, provinceName) || 
-			equals(el.sigla_provincia, provinceName) || 
-			equals(el.denominazione_provincia, provinceName)
-		);
+	return JSON.parse(decodeBase64(data.content)).filter(
+		(el) =>
+			equals(`${el.codice_provincia}`, provinceName) ||
+			equals(el.sigla_provincia, provinceName) ||
+			equals(el.denominazione_provincia, provinceName),
+	);
 };
 
 export const fetchProvincesData = async (): Promise<ProvincesResponse> => {
--- api/regions/index.ts
+++ api/regions/index.ts
@@ -1,12 +1,19 @@
-import { NowRequest, NowResponse } from '@now/node'
-import { fetchRegionsData } from '../_util/agent'
-import { cacheFunction } from '../_util/cache'
+import { NowRequest, NowResponse } from '@now/node';
+import { fetchRegionsData, fetchRegionsDataSimple } from '../_util/agent';
+import { cacheFunction } from '../_util/cache';
 
 export default async (req: NowRequest, res: NowResponse) => {
-    try {
-		const response = await cacheFunction('15m')(fetchRegionsData);
+	try {
+		let response: any = null;
+
+		if (req.query.plain == 'true') {
+			response = await cacheFunction('15m')(fetchRegionsDataSimple);
+		} else {
+			response = await cacheFunction('15m')(fetchRegionsData);
+		}
+
 		return res.send(response);
 	} catch (error) {
 		return res.status(500).send(error);
 	}
-}
+};
